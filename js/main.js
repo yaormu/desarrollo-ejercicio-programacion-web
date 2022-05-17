fc7cb01b7d61d63d3257c2663bba6fd75d1a3a5c
yamid <yaormu@gmail.com>
2022-05-17 17:56:30 -0500

    Se anexa JavaScript para tener 3 temas de colores para el sitio, con  una barra flotante que siempre estara al lado izquierdo donde al seleccionar el circulo de dicho color quedara la página y al navegar por las démas opciones mantendra este color

---
+++ js/main.js
@@ -0,0 +1,25 @@
+let color = document.getElementById("wrapper");
+
+const color1 = () => {
+    color.style.backgroundColor = "#191c32";
+    localStorage.setItem("colorete", "#191c32");
+}
+
+const color2 = () => {
+    color.style.backgroundColor = "#79334a";
+    localStorage.setItem("colorete", "#79334a");
+}
+
+const color3 = () => {
+    color.style.backgroundColor = "#30323f";
+    localStorage.setItem("colorete", "#30323f");
+}
+
+(() => {
+    const colorin =  localStorage.getItem("colorete");
+    if(colorin === null) {
+       color1(); 
+    }else {
+        color.style.backgroundColor = colorin;
+    }
+})();  
